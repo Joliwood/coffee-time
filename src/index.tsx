cea84465142096a400a7d50d7e7a74a77529961a
Guillaume <jolibois.contact@gmail.com>
2023-05-12 12:18:31 +0200

    :construction: MUI integrated + WIP custom palette color

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import FirstComponent from "./components/FirstComponent";
-import UserComponent from "./components/UserComponent";
+// import FirstComponent from "./components/FirstComponent";
+// import UserComponent from "./components/UserComponent";
 import "./index.scss";
-import "./reset.css";
+import Pricing from "./components/Pricing";
 
 ReactDOM.render(
   <body className="globalContainer">
-    <h1>Hello, Welcome to React and TypeScript</h1>
-    <FirstComponent />
+    {/* <FirstComponent />
     <UserComponent
       name="John Doe"
       age={26}
       address="87 Summer St, Boston, MA 02110"
       dob={new Date()}
-    />
+    /> */}
+    <Pricing />
   </body>,
   document.getElementById("root")
 );
