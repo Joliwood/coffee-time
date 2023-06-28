00b82bcd39e5aaf93a847bb49f30d72946f0c051
Guillaume <jolibois.contact@gmail.com>
2023-06-29 00:32:28 +0200

    :hammer: Jest working with only TS files

--- src/components/tests/redux.test.tsx
+++ src/components/tests/redux.test.tsx
@@ -3,25 +3,24 @@ import { render, cleanup, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import React from "react";
+import expect from "expect";
 import "@testing-library/jest-dom";
 
-// Changement de Jest 27 à 28, faire attention à ces lignes pour le passage en TS
-// - const expect = require('expect');
-// + const {expect} = require('expect');
-
 const mockStore = configureStore([]);
-const store = mockStore({ counter: [] });
+const store: any = mockStore({ counter: [] });
 
 afterEach(() => {
   cleanup();
 });
 
-describe("test in TS", () => {
-  render(
-    <Provider store={store}>
-      <Home />
-    </Provider>
-  );
+describe("Checking Redux", () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+  });
 
   it("should exists", () => {
     expect(screen.getByTestId("header")).not.toBeNull();
